1cb4dae625e6dc835c534f07ad53732f82f5dece
JuHeonHa <hjhjaueon97@naver.com>
2023-10-08 15:25:56 +0900

    feat : API 연동

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col py-10">
+    <div>
+      <h2>Nuxt Movies App</h2>
+    </div>
+    <div class="flex justify-center items-center h-32">
+      <input type="text" v-model="searchTerm" class="px-2 py-1 border border-gray-800 rounded-md min-w-64" />
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 self-center gap-x-10 gap-y-10 mb-10">
+      <div v-for="movie in data?.results">
+        {{ movie.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ApiResponse } from "../types/APIResponse";
+const searchTerm = ref("");
+const url = computed(() => `api/movies/search?query=${searchTerm.value}`);
+
+const { data } = await useFetch<ApiResponse>(url);
+</script>
+
+<style scoped></style>
